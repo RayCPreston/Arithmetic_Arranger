373547a16a136de859e8b00b8939fe2d554f7a8c
Ray Preston <raycpreston@gmail.com>
2020-11-27 13:42:34 -0600

    Initial commit

---
+++ Arithmetic_Arranger.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Get user input.  It should be in the format x + y or x - y
+operators = "+-x/%"
+equation = input("Input your equation in the format x +/- y: ")
+equation = equation.split(" ")
+while not equation[0].isnumeric() or equation[1] not in operators or not equation[2].isnumeric():
+    equation = input("Please make sure to input a valid equaltion: x =/- y: " )
+    equation = equation.split(" ")
+
+#Get the max length
+longestOperand = max(len(equation[0]), len(equation[2])) + 2
+#top line
+#add spaces to equal max length + 2
+topOperand = ""
+for i in range(longestOperand - len(equation[0])):
+    topOperand += " "
+topOperand += equation[0]
+print(topOperand)
+#print bottom operand
+bottomOperand = "+ "
+for i in range(longestOperand - len(equation[2]) - 2):
+    bottomOperand += " "
+bottomOperand += equation[2]
+print(bottomOperand)
+equalsLine = ""
+for i in range(longestOperand):
+    equalsLine += "_"
+print(equalsLine)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Arithmetic_Arranger
+ A simple project inspired by a free code camp python project
